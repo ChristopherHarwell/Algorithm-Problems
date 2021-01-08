22a49b8fb13a434f4fb6ba8452fd0c183fa0536f
chris <chris@manjaro.attlocal.net>
2021-01-08 09:32:46 -0500

    Implements recursive DFS of the BST

--- Binary Trees/branchSums.js
+++ Binary Trees/branchSums.js
@@ -31,33 +31,33 @@ const helperFunction = (root, res) => {
 }
 
 const tree = new BinaryTree(1)
-tree.left.value = 2;
-tree.left.left.value = 4;
-tree.left.right.value = 5;
+tree.left = 2;
+tree.left.left = 4;
+tree.left.right = 5;
 
-tree.left.right.left.value = 10;
-//  tree.left.right.right.value = null;
+tree.left.right.left = 10;
+//  tree.left.right.right = null;
 
-//  tree.left.right.left.left.value = null;
-//  tree.left.right.left.right.value = null;
+//  tree.left.right.left.left = null;
+//  tree.left.right.left.right = null;
 
 
-tree.left.left.left.value = 8;
-//  tree.left.left.left.left.value = null;
-//  tree.left.left.left.right.value = null;
+tree.left.left.left = 8;
+//  tree.left.left.left.left = null;
+//  tree.left.left.left.right = null;
 
-tree.left.left.right.value = 9;
-//  tree.left.left.right.left.value = null;
-//  tree.left.left.right.right.value = null;
+tree.left.left.right = 9;
+//  tree.left.left.right.left = null;
+//  tree.left.left.right.right = null;
 
-tree.right.value = 3;
-tree.right.left.value = 6;
-//  tree.right.left.left.value = null;
-//  tree.right.left.right.value = null;
+tree.right = 3;
+tree.right.left = 6;
+//  tree.right.left.left = null;
+//  tree.right.left.right = null;
 
-tree.right.right.value = 7;
-//  tree.right.right.left.value = null;
-//  tree.right.right.right.value = null;
+tree.right.right = 7;
+//  tree.right.right.left = null;
+//  tree.right.right.right = null;
 
 
 branchSums(tree);
